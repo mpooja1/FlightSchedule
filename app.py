fb76127cdbcb59836b32e84ae24b3826eab10040
mpooja <mpooja@pdx.edu>
2016-11-15 01:40:37 -0800

    first commit

--- app.py
+++ app.py
@@ -31,16 +31,19 @@ def makeWebhookResult(req):
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
-    zone = parameters.get("FlightNumber")
+    FlightNumber = parameters.get("FlightNumber")
+	AirLine = parameters.get("AirLine")
 
-    AirLine = {'113':"AjaxAir", '114':"AjaxAir", '121':"BakerAir", '122':"BakerAir", '124':"BakerAir", '522': 'CarsonAir','679':'CarsonAir','670':'CarsonAir','671':'CarsonAir','672':'CarsonAir'}
-  ##  DepartureCity = {'113':Portland, '114':Atlanta, '121':Atlanta, '122':NewYork, '124':Portland, '522': Portland,'679':NewYork,'670':NewYork,'671':Atlanta,'672':Portland}
-   # DepartureTime = {'113':'8:03AM', '114':'2:05PM', '121':'5:14PM', '122':'9:00PM', '124':'9:03AM', '522':'2:15AM','679':'9:30AM','670':'9:30AM','671':'1:20PM','672':'1:25PM'}
-   # ArrivalCity = {'113': Atlanta, '114': Portland, '121': NewYork, '122': Portland, '124': Atlanta, '522': NewYork, '679': Atlanta , '670': Portland , '671': NewYork , '672': NewYork}
-   # ArrivalTime = {'113': '12:51AM', '114': '4:44PM', '121': '7:20PM', '122': '12:13AM', '124': '12:52PM', '522': '4:58PM', '679': '11:30AM' , '670': '12:05PM' , '671': '2:55PM' , '672': '8:36PM'}
-    #ArrivalCity = {'113': landed, '114': boarding, '121': departed, '122': scheduled, '124': delayedto955, '522': scheduled, '679': departed , '670': departed , '671': scheduled , '672': scheduled}
-
-    speech = "The airline for " + zone + " is " + str(AirLine[zone]) + "."
+    AirLine = {'113':'AjaxAir', '114':'AjaxAir', '121':'BakerAir', '122':'BakerAir', '124':'BakerAir', '522': 'CarsonAir','679':'CarsonAir','670':'CarsonAir','671':'CarsonAir','672':'CarsonAir'}
+  DepartureCity = {'113':'Portland', '114':'Atlanta', '121':'Atlanta', '122':'NewYork', '124':'Portland', '522': 'Portland','679':'NewYork','670':'NewYork','671':'Atlanta','672':'Portland'}
+  DepartureTime = {'113':'8:03AM', '114':'2:05PM', '121':'5:14PM', '122':'9:00PM', '124':'9:03AM', '522':'2:15AM','679':'9:30AM','670':'9:30AM','671':'1:20PM','672':'1:25PM'}
+  ArrivalCity = {'113': 'Atlanta', '114': 'Portland', '121': 'NewYork', '122': 'Portland', '124': 'Atlanta', '522': 'NewYork', '679': 'Atlanta' , '670': 'Portland' , '671': 'NewYork' , '672': 'NewYork'}
+  ArrivalTime = {'113': '12:51AM', '114': '4:44PM', '121', '7:20PM', '122': '12:13AM', '124': '12:52PM', '522': '4:58PM', '679': '11:30AM' , '670': '12:05PM' , '671': '2:55PM' , '672': '8:36PM'}
+   Status = {'113': 'landed', '114': 'boarding', '121': 'departed', '122': 'scheduled', '124': 'delayedto9:55', '522': 'scheduled', '679': 'departed' , '670': 'departed' , '671': 'scheduled' , '672': 'scheduled'}
+   
+   
+    speech = "The airline for " + AirLine + " is " + str(FlightNumber[AirLine]) + "."
+  #  speech = "The airline for " + FlightNumber + " is " + str(AirLine[FlightNumber]) + "."
 
     print("Response:")
     print(speech)
