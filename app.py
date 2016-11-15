5214463aa3a37c4e12a4f8cb794b8a08c4d040c3
mpooja <mpooja@pdx.edu>
2016-11-15 01:03:01 -0800

    first commit

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def makeWebhookResult(req):
     ArrivalTime = {'113': '12:51AM', '114': '4:44PM', '121': '7:20PM', '122': '12:13AM', '124': '12:52PM', '522': '4:58PM', '679': '11:30AM' , '670': '12:05PM' , '671': '2:55PM' , '672': '8:36PM'}
     ArrivalCity = {'113': landed, '114': boarding, '121': departed, '122': scheduled, '124': delayedto955, '522': scheduled, '679': departed , '670': departed , '671': scheduled , '672': scheduled}
 
-    speech = "The airline for " + FlightNumber + " is " + str(AirLine[FlightNumber]) + "."
+    speech = "The airline for " + zone + " is " + str(AirLine[zone]) + "."
 
     print("Response:")
     print(speech)
